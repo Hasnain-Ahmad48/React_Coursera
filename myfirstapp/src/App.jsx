bf8ded0791b197bb9b890a82ec0c47830b9fe811
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-10-22 20:43:51 +0500

    simple project

--- myfirstapp/src/App.jsx
+++ myfirstapp/src/App.jsx
@@ -2,6 +2,7 @@ function App(props) {
   const currDate = new Date();
   return (
     <>
+    <h1>Hi From Hasnain</h1>
       <h1>Hello World</h1>
       <h2>today's date is {currDate.toLocaleDateString()} and time is {currDate.toLocaleTimeString()}</h2>
     </>
